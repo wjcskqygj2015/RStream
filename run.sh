ca00f1ad6b6bbfa01e53f3b20fff2bb12c352fe3
WuJiacheng <wjcskqygj@gmail.com>
2020-06-03 09:58:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-./cc ~/data/web-LiveJournal1.txt 64 0 64 9
+./cc ~/data/web-LiveJournal1.txt 64 0 64 17
 ./cc ~/data/web-orkut.txt 64 0 64 9
-./cc ~/data/arabic-2005.txt 64 0 64 ?
-./cc ~/data/twitter-shuffle.txt 64 0 64 ?
+./cc ~/data/arabic-2005.txt 64 0 64 52
+./cc ~/data/twitter-shuffle.txt 64 0 64 54
 
-./pagerank ~/data/web-LiveJournal1.txt 64 0 64 ?
-./pagerank ~/data/web-orkut.txt 64 0 64 ?
+./pagerank ~/data/web-LiveJournal1.txt 64 0 64 88
+./pagerank ~/data/web-orkut.txt 64 0 64 36
 ./pagerank ~/data/arabic-2005.txt 64 0 64 ?
 ./pagerank ~/data/twitter-shuffle.txt 64 0 64 ?
